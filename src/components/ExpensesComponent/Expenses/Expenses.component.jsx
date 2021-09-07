7e6566dd36572c6edd47bda5d3f77249d9576507
Job Ugbem <ugbemjob57@gmail.com>
2021-09-07 15:17:32 +0100

    Added filter method with its two way binding

--- src/components/ExpensesComponent/Expenses/Expenses.component.jsx
+++ src/components/ExpensesComponent/Expenses/Expenses.component.jsx
@@ -1,10 +1,29 @@
+import { useState } from 'react'
 import Card from '../../UI/Card/Card.components'
 import ExpenseItem from '../ExpenseItem/ExpenseItem.component'
+import ExpenseFilter from '../ExpenseFilter/ExpenseFilter.components'
 import './Expenses.styles.css'
-export const Expenses = ({ items }) => (
-  <Card className='expenses'>
-    {items.map(({ title, amount, date, ...item }) => (
-      <ExpenseItem key={item.id} title={title} amount={amount} date={date} />
-    ))}
-  </Card>
-)
+export const Expenses = ({ items }) => {
+  const [filteredYear, setFilteredYear] = useState('2021')
+  const filterSelectedYearHandler = selectedYear => {
+    setFilteredYear(selectedYear)
+  }
+  return (
+    <div>
+      <Card className='expenses'>
+        <ExpenseFilter
+          defaultYear={filteredYear}
+          onFilterYear={filterSelectedYearHandler}
+        />
+        {items.map(({ title, amount, date, ...item }) => (
+          <ExpenseItem
+            key={item.id}
+            title={title}
+            amount={amount}
+            date={date}
+          />
+        ))}
+      </Card>
+    </div>
+  )
+}
